59764d702e35ade35544a7f0519e1b4933a0eebb
Simon Kahan <simon.kahan@gmail.com>
2020-06-21 10:35:39 -0700

    initial commit

---
+++ split-vtp-files.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p $2
+cd $1
+for i in `ls *.pvtp`
+do
+    cp $i $2/$i.txt;
+done
+for i in `ls *.vtp`
+do
+    awk '{print; if ($1 == "<AppendedData") exit;}' $i > $2/$i.txt;
+    awk 'BEGIN{data = 0;}{if (data == 1) print; if ((data != 1) && ($1 == "<AppendedData")) {data = 1;}}' $i > $2/$i.bytes;
+done
