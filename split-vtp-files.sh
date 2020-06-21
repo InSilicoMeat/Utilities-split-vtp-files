a14a7f1ef10958cbb7bb5e4da8140bed865edcdf
Simon Kahan <simon.kahan@gmail.com>
2020-06-21 13:49:09 -0700

    changed suffix .bytes to .data.bytes suffix because unity doesn't differentiate filename .txt from .bytes

--- split-vtp-files.sh
+++ split-vtp-files.sh
@@ -8,5 +8,5 @@ done
 for i in `ls *.vtp`
 do
     awk '{print; if ($1 == "<AppendedData") exit;}' $i > $2/$i.txt;
-    awk 'BEGIN{data = 0;}{if (data == 1) print; if ((data != 1) && ($1 == "<AppendedData")) {data = 1;}}' $i > $2/$i.bytes;
+    awk 'BEGIN{data = 0;}{if (data == 1) print; if ((data != 1) && ($1 == "<AppendedData")) {data = 1;}}' $i > $2/$i.data.bytes;
 done
